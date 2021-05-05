84382759170c55967a77b806c17a570bde46fd07
niish03 <nishitpatel.nnp@gmail.com>
2021-05-05 19:04:56 +0530

    j

--- customjs.js
+++ customjs.js
@@ -11,18 +11,5 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-$(function(){ 
-  var navMain = $(".navbar-collapse"); // avoid dependency on #id
-  // "a:not([data-toggle])" - to avoid issues caused
-  // when you have dropdown inside navbar
-  navMain.on("click", "a:not([data-toggle])", null, function () {
-      navMain.collapse('hide');
-  });
-});
 
 
-$(function () {
-  $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
-          $('.navbar-toggle:visible').click();
-  });
-});
