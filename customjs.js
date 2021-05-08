99a55b4678059ae1cecc769eafeaa5d073cb0289
niish03 <nishitpatel.nnp@gmail.com>
2021-05-08 21:16:25 +0530

    update

--- customjs.js
+++ customjs.js
@@ -20,3 +20,17 @@ $(document).ready(function() {
       } 
   });  
 });
+
+
+let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
