72e76a75b7ab9d5d6a77f0e9a6a0f26fa2de2338
niish03 <nishitpatel.nnp@gmail.com>
2021-05-11 00:07:10 +0530

    update

--- customjs.js
+++ customjs.js
@@ -34,6 +34,7 @@ jQuery('#contactform').on('submit' , function(e){
       
       jQuery('#submit').attr("value","Send message");
       jQuery('#submit').css("background","#4154f1");
+      document.getElementsByClassName("sent-message").style("display","block");
 
   } 
 });
