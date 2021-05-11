4583afcebcc401fa669d047ce8471b4e719fde82
niish03 <nishitpatel.nnp@gmail.com>
2021-05-12 00:53:55 +0530

    Update (without firebase integration)

--- customjs.js
+++ customjs.js
@@ -71,3 +71,22 @@ let backtotop = select('.back-to-top')
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
+
+  const realFileBtn = document.getElementById("real-file");
+  const customBtn = document.getElementById("custom-button");
+  const customTxt = document.getElementById("custom-text");
+  
+  customBtn.addEventListener("click", function() {
+    realFileBtn.click();
+  });
+  
+  realFileBtn.addEventListener("change", function() {
+    if (realFileBtn.value) {
+      customTxt.innerHTML = realFileBtn.value.match(
+        /[\/\\]([\w\d\s\.\-\(\)]+)$/
+      )[1];
+      
+    } else {
+      customTxt.innerHTML = "No file chosen";
+    }
+  });
