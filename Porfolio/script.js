fc6e887a99cc8f210f77bee99fce63533e0e7ba4
NISHIT PATEL <61544937+niish03@users.noreply.github.com>
2021-06-07 18:52:17 +0530

    Add files via upload

---
+++ Porfolio/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing', {
+    strings:['Web developer', 'Flutter developer','UI/UX designer' , 'Graphic desiner','Freelancer' ,'Photographer' ],
+backSpeed: 50,
+typeSpeed: 50,
+loop:true
+  });
